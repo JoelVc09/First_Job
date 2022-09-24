dc92dfbdbbd1c82ea51dbe271580d4fda0f1988c
Usuario <Usuario@LAPTOP-L2HIARQP>
2022-09-23 22:11:27 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Persona
+    {
+        public int dni { get; set; }
+        public bool estado { get; set; }
+        public string nombre { get; set; }
+
+
+        public Persona( )
+        {
+
+        }
+
+
+
+
+    }
+}
